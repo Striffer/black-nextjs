2cb2ace90689e8a6174060d4eac8e15fb00ff94b
Pedro Lima <eng.pedrocarim@gmail.com>
2024-05-21 20:55:00 -0300

    Add API consumer and use it on a component ProductCard.

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,18 +1,21 @@
+import Header from "@/Components/Header";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Cart: NextPage = () => {
   return (
     <>
-        <Head>
-            <title>Carrinho</title>
-            <meta name="description" content="Meu carrinho de compras"/>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <h1>
-            Carrinho
-        </h1>
+      <Header />
+
+      <h1>
+        Carrinho
+      </h1>
 
     </>
   )
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,18 +1,44 @@
-import { NextPage } from "next";
+import Header from "@/Components/Header";
+import ProductsList from "@/Components/ProductsList";
+import { ProductType, fetchProducts } from "@/services/products";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { ReactNode } from "react";
+import { Container } from "reactstrap";
 
-const Products: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts()
+
+  return {
+    props: {
+      products
+    }
+  }
+}
+
+const Products: NextPage = (props: {
+  children?: ReactNode
+  products?: ProductType[]
+}) => {
   return (
     <>
-        <Head>
-            <title>Nossos Produtos</title>
-            <meta name="description" content="Conheça todos os nossos produtos"/>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça todos os nossos produtos"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <h1>
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
             Nossos Produtos
-        </h1>
+          </h1>
+
+          {<ProductsList products={props.products!} />}
+        </Container>
+      </main>
 
     </>
   )
